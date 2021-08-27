39e24408f3501d87480fc64156dfdfc5b01f1a46
Troy Palacino <tpalacino@users.noreply.github.com>
2021-08-27 08:38:49 -0400

    Added React TS App

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component<any, any> {
+  public render() {
+    return <div className="App">
+      <h1>My React App</h1>
+      <h2>Author: Troy Palacino</h2>
+    </div>;
+  }
+}
