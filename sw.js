8ff5bc2a0d1f8829feab367bf04ace9b464e6e2d
Troy Palacino <tpalacino@users.noreply.github.com>
2021-09-08 20:29:26 -0400

    Updates

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(() => {
-    console.log("app installed.")
-  })
-})
+const cacheName = "troys-desktop";
+const resourcesToPrecache = [
+  "/",
+  "index.html",
+  "static/js/bundle.js",
+  "static/js/vendors~main.chunk.js",
+  "static/js/main.chunk.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(resourcesToPrecache)));
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request) || fetch(event.request));
+});
